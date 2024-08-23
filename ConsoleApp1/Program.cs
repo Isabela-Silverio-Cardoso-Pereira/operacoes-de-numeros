8e9fc7435ae0092667962e327e815aeed4964aff
isabe <isabe@DESKTOP-7JLA0L9>
2024-08-23 13:28:25 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("digite o primeiro numero");
+        double num1;
+        double num2;
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("digite o segundo numero");
+        num2= Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("SOMA: " + (num1 + num2));
+        Console.WriteLine("SUBTRACAO " + (num1 - num2));
+        Console.WriteLine("DIVISAO " + (num1 / num2));
+        Console.WriteLine("MULTIPLICACAO" + (num1 * num2));
+
+        
+    }
+}
